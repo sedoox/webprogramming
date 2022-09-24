9ba1f4770fe024ce23b772ba5ef33a435988702f
Serdar Tuzcu <sedooxyt@gmail.com>
2022-09-23 22:33:46 -0500

    add pikachu webpage

---
+++ pikachu.js
@@ -0,0 +1,12 @@
+// Listener attached to the page
+window.addEventListener("load", init);
+
+function init() {
+    let demoButton = document.getElementById("demo-btn");
+    demoButton.addEventListener("click", changeImage);
+}
+
+function changeImage() {
+    let pokeballImg = document.getElementById("pokeball");
+    pokeballImg.src = "pikachu.gif"
+}
