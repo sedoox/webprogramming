8368fe8d28d1b822858a7620f29ae9bd2d9e9441
Serdar Tuzcu <sedooxyt@gmail.com>
2022-09-26 14:43:45 -0500

    adjusted rock paper scissors js

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -1,29 +1,29 @@
 function rockPaperScissors(player1, player2) {
-    player1.toLowerCase()
-    player2.toLowerCase()
-    if(checkInvalidMove(player1) && checkInvalidMove(player2)) {
+    player1 = player1.toLowerCase()
+    player2 = player2.toLowerCase()
+    if(!checkValidMove(player1) && !checkValidMove(player2)) {
         console.log("Invalid moves of " + player1 + " and " + player2 + "!")
         return
-    } else if (checkInvalidMove(player1)) {
+    } else if (!checkValidMove(player1)) {
         console.log("Invalid move of " + player1 + "!")
         return
-    } else if (checkInvalidMove(player2)) {
+    } else if (!checkValidMove(player2)) {
         console.log("Invalid move of " + player2 + "!")
         return
     }
 
     if (player1 == player2) {
-        console.log("tie")
+        console.log("TIE!")
         return
     }
 
     if (player1 == "rock" && player2 == "paper" || player1 == "paper" && player2 == "scissors" || player1 == "scissors" && player2 == "rock") {
-        console.log("PLayer 2 won")
+        console.log("PLayer 2 wins!")
     } else {
-        console.log("PLayer 1 won")
+        console.log("PLayer 1 wins!")
     }
 }
 
-function checkInvalidMove(move) {
+function checkValidMove(move) {
     return move == "rock" || move == "paper" || move == "scissors";
 }
