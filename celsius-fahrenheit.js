948cf35699e329975bc008ae96a85e52adeed838
Serdar Tuzcu <sedooxyt@gmail.com>
2022-10-06 21:56:34 -0500

    changed var to let

--- celsius-fahrenheit.js
+++ celsius-fahrenheit.js
@@ -1,9 +1,9 @@
 function fahrenheitToCelsius(fahrenheit) {
-    var celsius = (5/9) * (fahrenheit - 32)
+    let celsius = (5/9) * (fahrenheit - 32)
     console.log(celsius)
 }
 
 function celsiusToFahrenheit(celsius) {
-    var fahrenheit = (celsius*9)/5 + 32
+    let fahrenheit = (celsius*9)/5 + 32
     console.log(fahrenheit)
 }
